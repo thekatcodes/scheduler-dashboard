1171e8013ba8bccef7921e612e28ed8df37cebe1
Katie Liu <katieliu59@gmail.com>
2023-03-02 16:31:21 -0500

    Add Panels

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,19 +2,52 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+	{
+		id: 1,
+		label: "Total Interviews",
+		value: 6,
+	},
+	{
+		id: 2,
+		label: "Least Popular Time Slot",
+		value: "1pm",
+	},
+	{
+		id: 3,
+		label: "Most Popular Day",
+		value: "Wednesday",
+	},
+	{
+		id: 4,
+		label: "Interviews Per Day",
+		value: "2.3",
+	},
+];
 
 class Dashboard extends Component {
- state = { loading: true };
+	state = { loading: false };
 
-  render() {
-    const dashboardClasses = classnames("dashboard");
+	render() {
+		const dashboardClasses = classnames("dashboard");
 
-      if (this.state.loading === true) {
-          return <Loading />
-      }
-      
-    return <main className={dashboardClasses} />;
-  }
+		if (this.state.loading === true) {
+			return <Loading />;
+		}
+
+		const panels = data.map((panel) => (
+			<Panel
+				key={panel.id}
+				id={panel.id}
+				label={panel.label}
+				value={panel.value}
+			/>
+		));
+
+        return <main className={dashboardClasses}>{panels}</main>;
+	}
 }
 
 export default Dashboard;
